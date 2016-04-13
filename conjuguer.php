9fa3213b5ea9ad45f39ca4446870f74ef002592b
benjamindecastro <benjamindecastro@hotmail.fr>
2016-04-13 06:35:19 -0400

    conjuguer.php

---
+++ conjuguer.php
@@ -0,0 +1,19 @@
+<?php
+  $temps=$_POST["temps"];
+  $verbe=$_POST["verbe"];
+  $verbe=substr($verbe,0,-2);
+  if($temps=="present"){
+    
+  }
+  if($temps=="futur"){
+    
+  }
+  if (temps=="imparfait"){
+    echo "$verbe ais";
+    echo "$verbe ais";
+    echo "$verbe ait";
+    echo "$verbe ions";
+    echo "$verbe iez";
+    echo "$verbe aient"; 
+  }
+?>
