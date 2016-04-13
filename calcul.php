5fbdb2667d68d8d41952a56c650b01a16a44bb28
benjamindecastro <benjamindecastro@hotmail.fr>
2016-04-13 05:32:58 -0400

    reafficher le formulaire

--- calcul.php
+++ calcul.php
@@ -7,7 +7,7 @@
     <body>
         <?php
           if((isset($_POST['operand1']))&&(isset($_POST['operand2']))){
-            $operand1=$_POST['operand1'];
+            $operand1=$_POST['operand1'];  
             $operand2=$_POST['operand2'];
             $operation=$_POST['operation'];
             if ((is_numeric($operand1)==true)&&(is_numeric($operand2)==true)){
@@ -25,7 +25,7 @@
               }
               if($operation=="division"){
                 if ($operand2==0){
-                  echo "Impossible";
+                  echo "$operand1 / $operand2 = Impossible";
                 }
                 else{
                   $cal=$operand1/$operand2;
@@ -36,7 +36,7 @@
           }
         ?>
         <form method="post" action="calcul.php" enctype="encodage" >
-            <input type="text" placeholder="Operand1" name="operand1" required>
+            <input type="text" placeholder="Operand1" name="operand1" value="<?php if (isset($cal)) echo $cal; ?>" required>
             <select name="operation" required>
               <option value="soustraction">-</option>
               <option value="addition">+</option>
